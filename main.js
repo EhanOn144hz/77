8724ad5154abcc447eb5c57e77633df764b0e04c
EhanOn144hz <88392390+EhanOn144hz@users.noreply.github.com>
2024-02-05 20:44:32 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name = "Ehan";
+var last_name = " Hamid";
+var a = 15;
+var b = 6;
+var add = a+b;
+console.log(add)
+var subtract = a-b;
+console.log(subtract);
+var name = first_name + last_name;
+console.log(name);
